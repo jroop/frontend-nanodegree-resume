4ffbb806094ada7c8b3d56e9ae1aa02f31efe168
Joe Roop <joe.roop@gmail.com>
2015-07-28 21:22:09 -0700

    test: added P0 Javascript test problem
    Nothing fancy just following along

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,16 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+
+//Problem set #1
+ (function() {
+  var c =  299792458; //speed of light
+  var m2cm = 100; //meters to cm
+  var ns2s = 1.0/1000000000
+
+  var ans = c*(100)*(1/1000000000);
+  console.log(ans);
+  $('#main').append(ans);
+ })();
