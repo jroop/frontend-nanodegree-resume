388b8df91d1d38ada48e79025ded6acc76f5d105
Joe Roop <joe.roop@gmail.com>
2015-07-31 20:42:35 -0700

    feat: completed lesson 1 of javascript
    All code is here for the end of lesson 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -19,7 +19,7 @@ This is empty on purpose! Your code to build the resume will go here.
 
   var r = s.replace('awesome', 'fun');
 
-  $('#main').append(r);
+  //$('#main').append(r);
 
  })();
 
@@ -33,8 +33,8 @@ This is empty on purpose! Your code to build the resume will go here.
   var formattedName = HTMLheaderName.replace('%data%', name);
   var formattedRole = HTMLheaderRole.replace('%data%', role);
 
-  $('#header').prepend(formattedRole);
-  $('#header').prepend(formattedName);
+  //$('#header').prepend(formattedRole);
+  //$('#header').prepend(formattedName);
 })();
 
 /*
@@ -65,4 +65,79 @@ This is empty on purpose! Your code to build the resume will go here.
   var a = s.split(' ');
 
   console.log(a[0][0].toUpperCase()+ a[0].slice(1).toLowerCase() + ' ' + a[1].toUpperCase());
+})();
+
+/*
+  Add a bio to the page
+*/
+
+(function(){
+  var skills = [
+    "programming",
+    "rock climbing",
+    "skiing"
+  ];
+
+  //object leteral notation
+  var bio = {
+    "name": "Joe Roop",
+    "role": "Web Developer",
+    "contact": {
+      "email": "joe.roop@test.com",
+      "phone": "800-500-1234"
+    },
+    "picture": "https://scontent.fsnc1-1.fna.fbcdn.net/hprofile-xap1/v/t1.0-1/c80.0.160.160/p160x160/1526925_10152288547122018_1045643001_n.jpg?oh=45ddba4620df87a70abf024d90fcaa15&oe=5646E82B",
+    "message": "Hello and welcome to my world!",
+    "skills": skills
+  };
+
+  //dot notation
+  var work = {};
+  work.position = "software engineer";
+  work.employer = "Dell";
+  work.years = 2.1;
+  work.city = "Mountain View";
+
+  var edu = {};
+  edu['name'] = 'Georgia Tech';
+  edu['years'] = 2;
+  edu['city'] = 'Atlanta';
+  edu['degree'] = 'MS Aerospace Engineering';
+
+
+  var education = [
+    {
+      "name": "Georgia Tech",
+      "degree": "MS Aerospace Engineering",
+      "date": 2006,
+      "graduate": true
+    },
+    {
+      "name": "Embry Riddle",
+      "degree": "BS Aerospace Engineering",
+      "date": 2004,
+      "graduate": true
+    }
+  ]
+
+
+  $('#header').prepend(HTMLheaderRole.replace('%data%', bio.role));
+  $('#header').prepend(HTMLheaderName.replace('%data%', bio.name));
+
+  $("#topContacts").append(HTMLmobile.replace('%data%', bio.contact.phone));
+  $("#topContacts").append(HTMLemail.replace('%data%', bio.contact.email));
+  $('#header').append(HTMLbioPic.replace('%data%', bio.picture));
+
+  $('#header').append(HTMLwelcomeMsg.replace('%data%', bio.message));
+
+  $('#header').append(HTMLskillsStart);
+  for(var i = 0; i < bio.skills.length; i++){
+    console.log(skills[i]);
+    $('#header').append(HTMLskills.replace('%data%',bio.skills[i]));
+  }
+  $('#workExperience').append(HTMLworkStart);
+  $('#workExperience').append(HTMLworkEmployer.replace('%data%',work["employer"])+HTMLworkTitle.replace('%data%',work["position"]));
+
+  $('#education').append(HTMLschoolStart);
+  $('#education').append(HTMLschoolName.replace('%data%',edu.name)+HTMLschoolDegree.replace('%data%', edu.degree));
 })();
